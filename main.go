4683ecb0557ec7ec6ff3fd7c9cdfaf41073b65ed
Sonny Ochoa <0xsonny@gmail.com>
2022-06-22 17:31:47 -0700

    Created the basic structure of the signup page.

--- main.go
+++ main.go
@@ -14,11 +14,25 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
+		views.Must(views.ParseFS(
+			templates.FS,
+			"home.gohtml", "tailwind.gohtml",
+		))))
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
+		views.Must(views.ParseFS(
+			templates.FS,
+			"contact.gohtml", "tailwind.gohtml",
+		))))
 	r.Get("/faq", controllers.FAQ(
-		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
+		views.Must(views.ParseFS(
+			templates.FS,
+			"faq.gohtml", "tailwind.gohtml",
+		))))
+	r.Get("/signup", controllers.StaticHandler(
+		views.Must(views.ParseFS(
+			templates.FS,
+			"signup.gohtml", "tailwind.gohtml",
+		))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found.", http.StatusNotFound)
