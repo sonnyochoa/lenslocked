cedf56c332cc8fe823829efd6719ccf493afebed
Sonny Ochoa <0xsonny@gmail.com>
2022-09-04 12:11:05 -0700

    Created sample orders to populate the orders table.

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -80,19 +80,32 @@ func main() {
 	// }
 	// fmt.Println("User created. id =", id)
 
-	id := 2
-	row := db.QueryRow(`
-		SELECT name, email
-		FROM users
-		WHERE id=$1;`, id)
-	var name, email string
-	err = row.Scan(&name, &email)
-	if err == sql.ErrNoRows {
-		fmt.Println("Error, no rows!")
-	}
-	if err != nil {
-		panic(err)
-	}
+	// id := 2
+	// row := db.QueryRow(`
+	// 	SELECT name, email
+	// 	FROM users
+	// 	WHERE id=$1;`, id)
+	// var name, email string
+	// err = row.Scan(&name, &email)
+	// if err == sql.ErrNoRows {
+	// 	fmt.Println("Error, no rows!")
+	// }
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Printf("User information: name=%s, email=%s\n", name, email)
+	// fmt.Printf("User information: name=%s, email=%s\n", name, email)
+
+	userID := 1
+	for i := 1; i <= 5; i++ {
+		amount := i * 100
+		desc := fmt.Sprintf("Fake order #%d", i)
+		_, err := db.Exec(`
+			INSERT INTO orders(user_id, amount, description)
+			VALUES($1, $2, $3)`, userID, amount, desc)
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("Created fake orders.")
 }
