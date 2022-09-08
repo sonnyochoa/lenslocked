a8f25557a1dd60fc4485412e37d6106aeb2655ce
Sonny Ochoa <0xsonny@gmail.com>
2022-09-07 18:19:11 -0700

    Created new sql table for some simple twitter functionality, and the Go code to work with these new tables.

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -46,25 +46,25 @@ func main() {
 	fmt.Println("Connected...")
 
 	// Create a database
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS users (
-			id SERIAL PRIMARY KEY,
-			name TEXT,
-			email TEXT UNIQUE NOT NULL
-		);
+	// _, err = db.Exec(`
+	// 	CREATE TABLE IF NOT EXISTS users (
+	// 		id SERIAL PRIMARY KEY,
+	// 		name TEXT,
+	// 		email TEXT UNIQUE NOT NULL
+	// 	);
 
-		CREATE TABLE IF NOT EXISTS orders (
-			id SERIAL PRIMARY KEY,
-			user_id INT NOT NULL,
-			amount INT,
-			description TEXT
-		);
-	`)
+	// 	CREATE TABLE IF NOT EXISTS orders (
+	// 		id SERIAL PRIMARY KEY,
+	// 		user_id INT NOT NULL,
+	// 		amount INT,
+	// 		description TEXT
+	// 	);
+	// `)
 
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("TABLES CREATED...")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("TABLES CREATED...")
 
 	// Insert some data...
 	// name := "New User"
@@ -109,36 +109,130 @@ func main() {
 	// }
 	// fmt.Println("Created fake orders.")
 
-	type Order struct {
-		ID          int
-		UserID      int
-		Amount      int
-		Description string
-	}
-	var orders []Order
-	userID := 1
-	rows, err := db.Query(`
-		SELECT id, amount, description
-		FROM orders
-		WHERE user_id=$1`, userID)
+	// type Order struct {
+	// 	ID          int
+	// 	UserID      int
+	// 	Amount      int
+	// 	Description string
+	// }
+	// var orders []Order
+	// userID := 1
+	// rows, err := db.Query(`
+	// 	SELECT id, amount, description
+	// 	FROM orders
+	// 	WHERE user_id=$1`, userID)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer rows.Close()
+
+	// for rows.Next() {
+	// 	var order Order
+	// 	order.UserID = userID
+	// 	err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	orders = append(orders, order)
+	// }
+	// err = rows.Err()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println("Orders:", orders)
+
+	// * * * * * * * * * * * * * * * *
+
+	/*  *****************************
+	    Exercise: Twitter SQL
+	***************************** */
+	// Create a twitter database
+	// _, err = db.Exec(`
+	// 	CREATE TABLE IF NOT EXISTS tusers (
+	// 		id SERIAL PRIMARY KEY,
+	// 		name TEXT,
+	// 		email TEXT UNIQUE NOT NULL,
+	// 		created_at TIMESTAMP NOT NULL DEFAULT NOW()
+	// 	);
+
+	// 	CREATE TABLE IF NOT EXISTS tweets (
+	// 		id SERIAL PRIMARY KEY,
+	// 		user_id INT NOT NULL,
+	// 		content TEXT,
+	// 		created_at TIMESTAMP NOT NULL DEFAULT NOW()
+	// 	);
+	// `)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("TABLES CREATED...")
+
+	// _, err = db.Exec(`
+	// 	CREATE TABLE IF NOT EXISTS tweet_reply (
+	// 		id SERIAL PRIMARY KEY,
+	// 		parent_tweet_id INT NOT NULL,
+	// 		user_id INT NOT NULL,
+	// 		content TEXT,
+	// 		created_at TIMESTAMP NOT NULL DEFAULT NOW()
+	// 	);
+	// `)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("tweet_reply TABLE CREATED...")
+
+	// Insert data...
+	// name := "Twitter BOT"
+	// email := "bot@twitter.com"
+	// row := db.QueryRow(`
+	// 	INSERT INTO tusers (name, email)
+	// 	VALUES ($1, $2) RETURNING id;`, name, email)
+
+	// var id int
+	// err = row.Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("Twitter User created. id =", id)
+
+	// id := 2
+	// row := db.QueryRow(`
+	// 	SELECT name, email
+	// 	FROM tusers
+	// 	WHERE id=$1;`, id)
+	// var name, email string
+	// err = row.Scan(&name, &email)
+	// if err == sql.ErrNoRows {
+	// 	fmt.Println("Error, no rows!")
+	// }
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("Twitter User information: name=%s, email=%s\n", name, email)
+
+	// userID := 1
+	// tweet := "This is another fake tweet. Still don't know how to tweet"
+	// _, err = db.Exec(`
+	// 	INSERT INTO tweets(user_id, content)
+	// 	VALUES($1, $2)`, userID, tweet)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("Created sample tweets.")
+
+	// Retweet
+	parentTweetID := 1
+	userID := 2
+	tweet := "Reply to a fake tweet. R.E.P.L.Y."
+	_, err = db.Exec(`
+		INSERT INTO tweet_reply(parent_tweet_id, user_id, content)
+		VALUES($1, $2, $3)`, parentTweetID, userID, tweet)
 	if err != nil {
 		panic(err)
 	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var order Order
-		order.UserID = userID
-		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
-		if err != nil {
-			panic(err)
-		}
-		orders = append(orders, order)
-	}
-	err = rows.Err()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Orders:", orders)
+	fmt.Println("Created sample retweets.")
 }
