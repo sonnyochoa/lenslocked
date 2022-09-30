6112a2128b2afe3768372f87cee0274140b4749a
Sonny Ochoa <0xsonny@gmail.com>
2022-09-30 05:31:24 -0700

    Created method to process the user signin. Added route for process signin.

--- controllers/users.go
+++ controllers/users.go
@@ -42,3 +42,19 @@ func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
 	data.Email = r.FormValue("email")
 	u.Templates.SignIn.Execute(w, data)
 }
+
+func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email    string
+		Password string
+	}
+	data.Email = r.FormValue("email")
+	data.Password = r.FormValue("password")
+	user, err := u.UserService.Authenticate(data.Email, data.Password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wront", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User authenticated: %+v", user)
+}
--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
 	r.Get("/signin", usersC.SignIn)
+	r.Post("/signin", usersC.ProcessSignIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found.", http.StatusNotFound)
