67e6e30d0a837d7f2ed8854f43a52d1c894128a9
Sonny Ochoa <0xsonny@gmail.com>
2022-10-03 12:30:22 -0700

    Refactored the SessionService to use a constant for minimum bytes per token. Deleted session service code for the rand.go package which was refactored into the SessionService.

--- models/session.go
+++ models/session.go
@@ -3,9 +3,13 @@ package models
 import (
 	"database/sql"
 	"fmt"
-	"go/token"
 
 	"github.com/sonnyochoa/lenslocked/rand"
+)
+
+const (
+	// The minimum number of bytes to be used for each session token.
+	MinBytesPerToken = 32
 )
 
 type Session struct {
@@ -20,17 +24,25 @@ type Session struct {
 
 type SessionService struct {
 	DB *sql.DB
+	// BytesPerToken is used to determine how many bytes to use when generating each
+	// session token. If this value is not set or is less than the MinBytesPerToken
+	// const it will be ignored and MinBytesPerToken will be used.
+	BytesPerToken int
 }
 
-func (ss *SessionService) Create(userID int) (*Session, error) {\
-	token, err := rand.SessionToken()
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	bytesPerToken := ss.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+	token, err := rand.String(bytesPerToken)
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
 	// TODO: Hash the session token
-	session := Session {
+	session := Session{
 		UserID: userID,
-		Token: token,
+		Token:  token,
 		// TODO: Set the TokenHash
 	}
 	// TODO: Store the session in our DB
