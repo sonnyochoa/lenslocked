e74399a48b6a4388cf0514c35604fe394403b606
Sonny Ochoa <0xsonny@gmail.com>
2022-10-03 15:45:11 -0700

    Added code to insert a valid session into the db.

--- models/session.go
+++ models/session.go
@@ -41,14 +41,20 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
-	// TODO: Hash the session token
 	session := Session{
 		UserID:    userID,
 		Token:     token,
 		TokenHash: ss.hash(token),
-		// TODO: Set the TokenHash
 	}
 	// TODO: Store the session in our DB
+	row := ss.DB.QueryRow(`
+		INSERT INTO sessions (user_id, token_hash)
+		VALUES ($1, $2)
+		RETURNING id;`, session.UserID, session.TokenHash)
+	err = row.Scan(&session.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
 	return &session, nil
 }
 
